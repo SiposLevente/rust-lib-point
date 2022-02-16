4f75b18a7830946c6d0f8eb8b3a7b24d89488899
SiposLevente <sipi.levente@gmail.com>
2022-02-16 21:32:28 +0100

    refactored, and added display feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 pub mod point {
+    use std::fmt::{Display, Formatter, Result};
+    use std::ops::{AddAssign,Sub,Add,Mul};
+
     pub struct Point<T> {
         pub x: T,
         pub y: T,
@@ -11,7 +14,7 @@ pub mod point {
 
         pub fn offset(&mut self, x: T, y: T)
         where
-            T: std::ops::AddAssign,
+            T: AddAssign,
         {
             self.x += x;
             self.y += y;
@@ -19,9 +22,9 @@ pub mod point {
 
         pub fn distance(&self, other_point: Point<T>) -> T
         where
-            T: std::ops::Sub<Output = T>
-                + std::ops::Add<Output = T>
-                + std::ops::Mul<Output = T>
+            T: Sub<Output = T>
+                + Add<Output = T>
+                + Mul<Output = T>
                 + Copy,
         {
             let point = Point::new(self.x, self.y);
@@ -31,15 +34,15 @@ pub mod point {
 
         pub fn to_string(&self) -> String
         where
-            T: std::fmt::Display,
+            T: Display,
         {
             format!("(x:{}, y:{})", self.x, self.y)
         }
     }
 
-    impl<T> std::ops::Add for Point<T>
+    impl<T> Add for Point<T>
     where
-        T: std::ops::Add<Output = T>,
+        T: Add<Output = T>,
     {
         type Output = Self;
 
@@ -51,9 +54,9 @@ pub mod point {
         }
     }
 
-    impl<T> std::ops::Sub for Point<T>
+    impl<T> Sub for Point<T>
     where
-        T: std::ops::Sub<Output = T>,
+        T: Sub<Output = T>,
     {
         type Output = Self;
 
@@ -71,6 +74,15 @@ pub mod point {
     {
         fn eq(&self, other: &Self) -> bool {
             self.x == other.x && self.y == other.y
+        }
+    }
+
+    impl<T> Display for Point<T>
+    where
+        T: Display,
+    {
+        fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+            write!(f, "({},{})", self.x, self.y)
         }
     }
 }
